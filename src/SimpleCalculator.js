dfe1be4c101ca39791e69cc781b7a0fb94758108
kueselj <kuesel.j@husky.neu.edu>
2021-07-10 15:07:44 -0400

    adding all

--- src/SimpleCalculator.js
+++ src/SimpleCalculator.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
 import { useState } from 'react';
 import calculateOneRepMax from "./CalculateOneRepMax"
+import convert from "./Convert"
 
 function SimpleCalculator() {
 
-    const [oneRepMax, setOneRepMax] = useState(1);
     const [unit, setUnit] = useState(false);
+
     const [weightLifted, setWeightLifted] = useState(1);
     const [repCount, setRepCount] = useState(1);
+
+    const [oneRepMax, setOneRepMax] = useState(1);
     
     
+    function toggleWeight(unit) {
+        setUnit(!unit)
+        
+    }
     
     function updateWeightLifted(weight) {
         setWeightLifted(weight)
@@ -31,40 +38,44 @@ function SimpleCalculator() {
   
     return (
 
-        
-        <div class = "calculator-container">
+        <div>
+            <div class = "calculator-container">
 
-            <input type="checkbox" onChange={() => setUnit(!unit)}/>
-            
-            {thing}
+                {/* <input type="checkbox" onChange={() => toggleWeight(unit)}/>
+                
+                {thing} */}
+                
+                <div class = "weight-container">
+                    
+                    <div class = "weight-count">
+                        <input label = "enter" class = "weight-input"  defaultValue = "1" type="number" onChange={event => updateWeightLifted(event.target.value)}/>
+                        
+                    </div>
+                    {/* <div>
+                        <input class="weight-slider" defaultValue = "1" type="range" min="1" max="500" onChange={event => updateWeightLifted(event.target.value)}></input>  
+                    </div> */}
+                    <div class = "weight-label">
+                        Weight Lifted (Lbs)
+                    </div>
+                </div>
+
+                <div class = "rep-container">
+                    
+                    <div class = "rep-count">
+                        {repCount}
+                    </div>
+                    <div>
+                        <input class="rep-input" defaultValue = "1" type="range" min="1" max="20" onChange={event => updateRepCount(event.target.value)}></input>  
+                    </div>
+                    <div class = "rep-label">
+                        Reps Performed
+                    </div>
+                </div>
+                
+            </div>
             <div class = "orm-label">
-                One Rep Max: {oneRepMax} {thing}
-            </div>
-            <div class = "weight-container">
-                
-                <div class = "weight-count">
-                    {weightLifted}
-                </div>
-                <div>
-                    <input class="weight-input" defaultValue = "1" type="range" min="1" max="500" onChange={event => updateWeightLifted(event.target.value)}></input>  
-                </div>
-                <div class = "weight-label">
-                    Weight Lifted
-                </div>
-            </div>
-
-            <div class = "rep-container">
-                
-                <div class = "rep-count">
-                    {repCount}
-                </div>
-                <div>
-                    <input class="rep-input" defaultValue = "1" type="range" min="1" max="20" onChange={event => updateRepCount(event.target.value)}></input>  
-                </div>
-                <div class = "rep-label">
-                    Reps Performed
-                </div>
-            </div>  
+                    One Rep Max: {oneRepMax} {thing}
+                </div>  
          </div>
     );
   }
